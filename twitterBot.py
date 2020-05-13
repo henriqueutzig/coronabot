65ecdbfe545cde7e4b2b89bc8f06fa24cc9a11b4
Henrique Utzig <henriqueutzig@gmail.com>
2020-05-13 12:36:17 -0300

    restructured tweet text, adding the data source and started new function 'isDuplicated'

--- twitterBot.py
+++ twitterBot.py
@@ -18,32 +18,23 @@ def postTweet(bot, data):
     deaths = data['deaths']['value']
     recovered = data['recovered']['value']
 
-    if(os.path.isfile('data.json')):
-        with open('data.json', 'r') as rj:
-            rj_file = rj.read()
-        oldData = json.loads(rj_file)
-        diff_confirmed = confirmed - oldData['confirmed']['value']
-        diff_deaths = deaths - oldData['deaths']['value']
-        diff_recoverd = recovered - oldData['recovered']['value']
-
-
-    
-
     if(confirmed > 0 and deaths > 0 and recovered > 0):
-        tweet = "CORONA VIRUS NO BRASIL: \n"
-        tweet += f"Infectados: {confirmed:,} \nMortos: {deaths:,} \nRecuperados: {recovered:,} \n"
-        if(diff_confirmed > 0 and len(tweet) < 280):
-            tweet += f"\n{diff_confirmed:,} novos infectados"
-        if(diff_deaths > 0 and len(tweet) < 280):
-            tweet += f"\n{diff_deaths:,} novas mortes"
-        if(diff_recoverd > 0 and len(tweet) < 280):
-           tweet += f"\n{diff_recoverd:,} novos recuperados"
-
-    if((tweet != "") and (len(tweet) <= 280)):
+        tweet = "COVID-19 VIRUS NO BRASIL\n\n"
+        tweet += f"Infectados: {confirmed:,}\nMortos: {deaths:,}\nRecuperados: {recovered:,}\n\n"
+        tweet += "Fonte dos Dados: John Hopkins University CSSE\n"
+    
+    if((tweet != "") and (len(tweet) <= 280) and (isDuplicated == False)):
         print(tweet + "\ntamanho:" + str(len(tweet)))
         bot.update_status(tweet)
 
 
+def isDuplicated(tweet):
+    isDup = True
+    #request old tweets
+
+    #compare old tweets with new and set isDup to True or False
+
+    return isDup
 
 def startup():
     # Authenticate to Twitter
