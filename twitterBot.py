17812150a854ec3f4061cce07ce9cef00673cfa8
Henrique Utzig <henriqueutzig@gmail.com>
2020-05-15 13:10:44 -0300

    added 'isDuplicated' (not working) and 'dataDiff', as the first function is not working the line 63 prevents dataDiff bugs that used to happen

--- twitterBot.py
+++ twitterBot.py
@@ -21,21 +21,56 @@ def postTweet(bot, data):
     if(confirmed > 0 and deaths > 0 and recovered > 0):
         tweet = "COVID-19 NO BRASIL\n\n"
         tweet += f"Infectados: {confirmed:,}\nMortos: {deaths:,}\nRecuperados: {recovered:,}\n\n"
-        tweet += "Fonte dos Dados: John Hopkins University CSSE\n"
-    
-    if((tweet != "") and (len(tweet) <= 280)): # and (isDuplicated == False)
+        tweet += dataDiff(data)
+        tweet += "\nFonte dos Dados: John Hopkins University CSSE\n"
+        tweet += "#FiqueEmCasa"
+    if((tweet != "") and (len(tweet) <= 280)): #and (isDuplicated(tweet, bot) == False)
         print(tweet + "\ntamanho:" + str(len(tweet)))
         bot.update_status(tweet)
 
 
-def isDuplicated(tweet):
+def isDuplicated(tweet, api):
+    ''' isDuplicated is not working, I have no idea why'''
     isDup = True
+    rTweet = repr(tweet)
     #request old tweets
+    oldTweets = api.user_timeline()
+    tweetList = []
 
+    for t in oldTweets:            
+        tweetList.append(t.text)
     #compare old tweets with new and set isDup to True or False
-
+    if (rTweet not in tweetList):
+        isDup = False
+    
     return isDup
 
+# function that gets the difference between the current Data and
+# the data stored in 'data.json' and return a stg to be added on the tweet
+def dataDiff(currentData):
+    diffTxt = ""
+    confirmed = currentData['confirmed']['value']
+    deaths = currentData['deaths']['value']
+    recovered = currentData['recovered']['value']
+    
+    if(os.path.isfile('data.json')):
+        with open('data.json', 'r') as rj:
+            rj_file = rj.read()
+        oldData = json.loads(rj_file)
+        diff_confirmed = confirmed - oldData['confirmed']['value']
+        diff_deaths = deaths - oldData['deaths']['value']
+        diff_recoverd = recovered - oldData['recovered']['value']
+    if ((diff_confirmed != confirmed) and (diff_deaths != deaths) and (diff_recoverd != recovered)): 
+        if(diff_confirmed > 0 and len(diffTxt) < 280):
+            diffTxt += f"{diff_confirmed:,} novos infectados"
+        if(diff_deaths > 0 and len(diffTxt) < 280):
+            diffTxt += f"\n{diff_deaths:,} novas mortes"
+        if(diff_recoverd > 0 and len(diffTxt) < 280):
+           diffTxt += f"\n{diff_recoverd:,} novos recuperados"
+    
+    return diffTxt
+
+# startup twitters api calling 
 def startup():
     # Authenticate to Twitter
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
