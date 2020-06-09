e725f640ba42a6fd1b6a77ac6af1808efac671c0
Henrique Utzig <henriqueutzig@gmail.com>
2020-06-09 12:57:28 -0300

    migrated to brasil.io API, since the JH one wasnt being update very often created a global varible that will store da data as a DataFrame created 2 function that will return the total of deaths and confirmed cases created 2 other functions that will return the numbers of deaths and confirmed cases by state (only will be implemented later when multi-threading has been implemented)

--- coronaTrackerAPI.py
+++ coronaTrackerAPI.py
@@ -1,23 +1,34 @@
-# Getting data from:  'https://covid19.mathdro.id/api'
-import requests 
+# Getting data from:  brasil.io
+import requests
 import json
+import pandas as pd 
 
-import datetime
-import os
-from database import storeData
-
-# Get Date 
-currentDate = datetime.datetime.now().strftime('%Y-%m-%d')
+global dfData
 
 def getLatestData():
 
-    url = "https://covid19.mathdro.id/api/countries/BR"  # "https://api.covid19api.com/summary"
+    url = 'https://brasil.io/api/dataset/covid19/caso/data/?format=json&is_last=True&place_type=state'
 
-    payload = {}
-    headers= {}
-
-    response = requests.request("GET", url, headers=headers, data = payload)
+    apiResponse = requests.request("GET", url)
     
-    responseJson = json.loads(response.text)
+    responseJson = json.loads(apiResponse.text)
+    jsonList = responseJson['results']
+
+    global dfData
+    dfData = pd.DataFrame.from_records(jsonList)
 
     return responseJson
+
+def getTotalDeaths():
+    return dfData['deaths'].sum()
+
+def getTotalInfected():
+    return dfData['confirmed'].sum()
+
+#def getStateDeaths(state):
+#
+#    return deaths
+#
+#def getStateInfected(state):
+#
+#    return infected
