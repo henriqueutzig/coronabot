525d4270739dafbe7073cd0a14fb1e287d4392b2
Henrique Utzig <henriqueutzig@gmail.com>
2020-09-03 16:18:24 -0300

    finished refactoring everything to use OOP

--- src/tweet.py
+++ src/tweet.py
@@ -4,23 +4,23 @@ class Tweet:
     
     def __init__(self, totalDeaths, totalInfected):
         if(totalInfected > 0 and totalDeaths > 0):
-            self.tweet = "COVID-19 NO BRASIL\n\n"
-            self.tweet += f"Infectados: {totalInfected:,}\nMortos: {totalDeaths:,}\n\n"
-            self.tweet += "Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e equipe de voluntários Brasil.IO\n"
-            self.tweet += "#FiqueEmCasa"
+            self.text = "COVID-19 NO BRASIL\n\n"
+            self.text += f"Infectados: {totalInfected:,}\nMortos: {totalDeaths:,}\n\n"
+            self.text += "Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e equipe de voluntários Brasil.IO\n"
+            self.text += "#FiqueEmCasa"
 
     def update(self, totalDeaths, totalInfected):
-        self.tweet = ""
+        self.text = ""
         if(totalInfected > 0 and totalDeaths > 0):
-            self.tweet = "COVID-19 NO BRASIL\n\n"
-            self.tweet += f"Infectados: {totalInfected:,}\nMortos: {totalDeaths:,}\n\n"
-            self.tweet += "Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e equipe de voluntários Brasil.IO\n"
-            self.tweet += "#FiqueEmCasa"
+            self.text = "COVID-19 NO BRASIL\n\n"
+            self.text += f"Infectados: {totalInfected:,}\nMortos: {totalDeaths:,}\n\n"
+            self.text += "Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e equipe de voluntários Brasil.IO\n"
+            self.text += "#FiqueEmCasa"
 
     def isDuplicated(self, oldTweets):
         isDup = True
         # remove current tweet footer 
-        currentTweet = self.tweet[:self.tweet.find("Fonte")]
+        currentTweet = self.text[:self.text.find("Fonte")]
         # remove old tweet's footer 
         tweetsText = []
         for t in oldTweets:
